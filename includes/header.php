cea308a0e42d3bbc35c5496529c1f59097fd7e39
Brian McKenna <D00197352@student.dkit.ie>
2021-03-05 18:46:20 +0000

    google font added

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,11 @@
 <!-- the head section -->
 <head>
-<title>My PHP CRUD App</title>
+<title>News Blog</title>
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="css/mystyle.css">
 </head>
 
 <!-- the body section -->
 <body>
-<header><h1>My PHP CRUD app</h1></header>
+<header><h1>My News Blog</h1></header>
